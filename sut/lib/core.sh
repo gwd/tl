10cddaf1396dd007337534e2705a126e1cd6c662
George Dunlap <george.dunlap@eu.citrix.com>
2014-11-10 19:23:57 +0000

    htlib: Rewrite htlib, implement tbz-to-image functionality
    This may be a template to re-organize the other library as well.
    
    Signed-off-by: George Dunlap <george.dunlap@eu.citrix.com>

---
+++ sut/lib/core.sh
@@ -0,0 +1,194 @@
+#!/bin/bash
+
+TESTLIB_HELP=()
+
+arg_parse_cmd=\
+"local -a args;
+local _a;
+local _vn;
+local _m;
+
+_m=true;
+
+for _a in \"\$@\" ; do
+    false && echo \"Evaluating \${_a} [[ \"\${_a/=}\" = \"\${_a}\" ]]\";
+    if \$_m && [[ \"\${_a/=}\" != \"\${_a}\" ]] ; then
+        false && echo Parameter;
+        _vn=\${_a%%=*};
+        eval \"local \$_vn\";
+        eval \"\$_a\";
+    else
+        false && echo Argument;
+        _m=false;
+        args+=(\"\$_a\");
+    fi;
+done"
+
+arg_parse="eval $arg_parse_cmd"
+
+function die()
+{
+   echo FATAL $@
+   exit 1
+}
+
+function fail()
+{
+   echo FATAL $@
+   [[ -n "$fail_cleanup" ]] && $fail_cleanup
+   exit 1
+}
+
+function info()
+{
+   echo INFO $@ 1>&2
+}
+
+function error()
+{
+   echo ERROR $@ 1>&2
+}
+
+function status()
+{
+   echo STATUS $@ 1>&2
+   $status_popup && (zenity --info --text="$@" &)
+   return 0
+}
+
+function parse-separator-array()
+{
+    local _pca_array_var="$2"
+    local _pca_list="$3"
+    local -a _pca_internal
+    local OLD_IFS
+
+    OLD_IFS=${IFS}
+    IFS="$1"
+    _pca_internal=($_pca_list)
+    IFS="${OLD_IFS}"
+
+    eval "${_pca_array_var}=(${_pca_internal[@]})"
+}
+
+function parse-comma-array()
+{
+    parse-separator-array "," "$1" "$2"
+}
+
+function test-parse-comma-array()
+{
+    local vars
+    local out
+    local i
+
+    vars="a,b,c"
+    parse-comma-array out $vars
+
+    for i in ${out[@]} ; do
+	echo "X $i";
+    done
+}
+
+function test-parse-colon-array()
+{
+    local vars
+    local out
+    local i
+
+    vars="kodo2:c6-test:c6-vm"
+    parse-separator-array ":" out $vars
+
+    for i in ${out[@]} ; do
+	echo "X $i";
+    done
+}
+
+function parse-config-array()
+{
+    local _pcfga_array_var="$1"
+    local _pcfga_reset_array_var="$2"
+    local _pcfga_list="$3"
+    local -a _pcfga_internal
+    local -a _pcfgr_internal
+    local _j
+    local _varname
+    local _value
+
+    echo "About to parse list"
+    parse-comma-array _pcfga_internal ${_pcfga_list}
+
+    echo Reset values
+    for _j in ${_pcfga_internal[@]} ; do
+	_varname=$(expr match "${_j}" '\([^=]*\)')
+	_value=$(eval echo \$$_varname)
+	echo " $_varname=$_value"
+	_pcfgr_internal=(${_pcfgr_internal[@]} "${_varname}=${_value}")
+    done
+
+    eval "${_pcfga_array_var}=(${_pcfga_internal[@]})"
+    eval "${_pcfga_reset_array_var}=(${_pcfgr_internal[@]})"
+}
+
+# Pass in either the current function name, or the name of the script
+requireargs="eval _func=\"\$FUNCNAME\" ; eval [[ -n \\\"\$_func\\\" ]] || _func=\$0 ; eval _require-args \$_func"
+
+function _require-args()
+{
+    local _arg
+    local _args
+    local fail_popup
+
+    _args=($@)
+
+    fail_popup=false
+
+    for _arg in ${_args[@]:1} ; do
+	eval "[[ -n \"\${$_arg}\" ]] || fail \"${_args[0]}: Missing $_arg\""
+    done
+}
+
+# Used to make a local cfg_ variable overriding the global one if
+# a given argument is given; otherwise, set the local variable to the
+# global one.
+#
+# For example, you can pass isosr_path to host-install, and it will
+# create a local variable cfg_isosr_path and set it to iso-sr, so that
+# host-install-post will get that value without having to pass it in;
+# Alternately, you can set iso_sr to cfg_isosr_path.
+#
+# Use like this: cfg_override [global_var] [local_var] ; eval $ret_eval
+function cfg_override()
+{
+    unset ret_eval
+    if eval "[[ -n \"\$$2\" ]]" ; then
+	ret_eval="local $1=\$$2"
+    else
+	ret_eval="$2=\$$1"
+    fi
+}
+
+function help()
+{
+    for i in "${TESTLIB_HELP[@]}" ; do
+	echo "$i"
+    done
+}
+
+function main()
+{
+    local cmd;
+
+    if [[ "$#" -eq "0" ]] ; then
+	echo "Usage: $0 function-name [arguments...]"
+	exit 1
+    fi
+
+    $arg_parse
+    info Running "${args[0]}"
+    "${args[0]}" "${args[@]:1}" || exit 1
+
+    if ! [[ -z "$RET" ]] ; then
+	echo $RET
+    fi
+}
---
+++ sut/lib/image.sh
@@ -0,0 +1,153 @@
+#!/bin/bash
+set -ex
+
+function vhd-create()
+{
+    vhd-util create -n $image -s $size    
+}
+
+function raw-create()
+{
+    truncate -s "${size}"M "$image"
+}
+
+function qcow2-create()
+{
+    qemu-img create -f qcow2 "$image" "${size}"M
+}
+
+function tb-c6-post()
+{
+    # For now, disable selinux.  Alternate would be "fixfiles relabel"
+    sed -i --follow-symlinks "s/SELINUX=enforcing/SELINUX=permissive/;" $mount/etc/selinux/config
+    chroot $mount fixfiles -f relabel
+
+    # Replace <UUID> with uuid in /etc/fstab, /etc/grub.conf
+    uuid=$(blkid /dev/$devp | perl -ne '/UUID="([a-f0-9-]*)"/; print $1;')
+
+    [[ -n "$uuid" ]] 
+
+    sed -i --follow-symlinks "s/<UUID>/$uuid/;" $mount/etc/fstab
+    sed -i --follow-symlinks "s/<UUID>/$uuid/;" $mount/boot/grub/grub.conf
+
+    # We expect this to fail, but it will copy the needed files for the next step
+    set +e
+    chroot $mount grub-install /dev/xvda
+    set -e
+
+    chroot $mount /sbin/grub --batch <<EOF
+device (hd0) /dev/xvda
+root (hd0,0)
+setup (hd0)
+quit
+EOF
+    ls -l $image
+
+
+    # Run grubby
+    vmlinuz=$(ls $mount/boot/vmlinuz-* | tail -1 | sed "s|$mount||;")
+    initramfs=$(ls $mount/boot/initramfs-* | tail -1 | sed "s|$mount||;")
+
+    echo vmlinuz $vmlinuz
+    echo initramfs $initramfs
+
+    chroot $mount grubby --bad-image-okay --add-kernel=$vmlinuz --initrd=$initramfs --title="CentOS" --make-default --copy-default
+
+    cat $mount/boot/grub/grub.conf
+    ls -l $image
+
+    # Set up networking
+    cat >$mount/etc/sysconfig/network <<EOF
+NETWORKING=yes
+HOSTNAME=centos.localdomain
+EOF
+
+    cat >$mount/etc/sysconfig/network-scripts/ifcfg-eth0 <<EOF
+DEVICE=eth0
+ TYPE=Ethernet
+ ONBOOT=yes
+ BOOTPROTO=dhcp
+EOF
+
+}
+
+function tbz-to-image()
+{
+    $arg_parse
+
+    ${wdir:=/tmp}
+    ${odir:=/images}
+    ${basename:=c6}
+    ${format:=vhd}
+    ${size:=2048}
+    ${dev:=xvda}
+
+    if [[ -e /dev/${dev} ]] ; then
+	echo /dev/${dev} exists!
+	exit 1
+    fi
+
+    mount=$wdir/centos-chroot
+    tarball=$odir/c6.tar.gz
+
+    case $format in
+	vhd|raw|qcow2)
+	    image=$odir/$basename-NN.$format
+	    ;;
+	*)
+	    echo Unknown imagetype $imagetype!
+	    exit 1
+	    ;;
+    esac
+
+    devp="${dev}1"
+
+    # Create empty image
+    rm -f $image
+    $format-create
+
+    # Attach image to dom0
+    xl block-attach 0 vdev=$dev,format=$format,target=$image
+    # !!!!
+    usleep 100000
+
+    # Make partitions
+    parted -a optimal /dev/$dev mklabel msdos
+
+    parted -a optimal -- /dev/$dev unit compact mkpart primary ext3 "1" "-1" 
+
+    # Make filesystem
+    mkfs.ext4 /dev/$devp
+
+    # Mount filesystem
+    #  - procfs, sysfs, mount, /dev/xvd*
+    rm -rf $mount
+    mkdir -p $mount
+    mount /dev/$devp $mount
+
+    ls -l $image
+
+    # Untar filesystem
+    tar xzp -f $tarball -C $mount
+    ls -l $image
+
+    # Make suitable for chroot
+    cp -a /dev/xvda* $mount/dev/
+
+    cat > $mount/etc/mtab <<EOF
+/dev/xvda1 / ext4 rw 0 0
+EOF
+
+    tb-c6-post
+
+    # Copy ssh keys
+    mkdir -p $mount/root/.ssh
+    cp /root/.ssh/authorized_keys $mount/root/.ssh/authorized_keys
+
+    # Clean up
+    umount $mount
+
+    xl block-detach 0 $dev
+    ls -l $image
+
+}
